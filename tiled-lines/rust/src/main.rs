506bf6cf03668c0beec2e65927b6aa4572bc1aa1
Garrett Smith <ggasmithh@users.noreply.github.com>
2019-07-17 21:02:36 -0500

    Got an (incorrect) result to display from Rust solution

--- tiled-lines/rust/src/main.rs
+++ tiled-lines/rust/src/main.rs
@@ -7,9 +7,7 @@ use image::Rgb;
 use imageproc::drawing::draw_line_segment_mut;
 use rand::Rng;
 
-let SIZE:u16 = 500;
-let STEP:u8 = 20;
-
+/*
 struct Context {
     current_x:u16;
     current_y:u16;
@@ -36,37 +34,35 @@ impl Context {
         &mut self.current_y
     }
 }
+*/
 
-fn draw (&mut img, &mut context:Context, x:u16, y:u16, width:u16, height:u16) {
-    let left_to_right:bool = rand::thread_rng().gen_range(0.0, 1.0) >= 0.5;
-
-    
-
-    if (left_to_right) {
-        draw_line_segment_mut(&mut img, (context.current_x() as f32, context.current_y() as f32), ((context.current_x() + width) as f32, (context.current_y() + height) as f32), Rgb([0u8, 0u8, 0u8]));
-        *context.current_x_mut() = u16::from(x + width);
-        //*context.current_y_mut() = u16::from(y + height);
-    } else{
-    draw_line_segment_mut(&mut img, ((context.current_x() + width) as f32, context.current_y() as f32), (context.current_x() as f32, (context.current_y() + height) as f32), Rgb([0u8, 0u8, 0u8]));
-        //*context.current_x_mut() = u16::from(x + width);
-        // the above may be wrong, so i'm taking it out
-        //*context.current_y_mut() = u16::from(y + height);
-    }
-    // putting this here to apply to all cases may also be wrong too
-    *context.current_y_mut() = u16::from(y + height);
-
-}
 
 fn main() {
-    // TODO: CREATE Image OBJECT
+    let size: u32 = 500;
+    let step: u8 = 20;
+    let mut img = image::ImageBuffer::new(size, size);
 
-    // TODO: IMPLEMENT THIS:
-    /* 
-    for (var x = 0; x < size; x += step) {
-        for (var y = 0; y < size; y += step) {
-            draw(x, y, step, step);
+    for (_, _, pixel) in img.enumerate_pixels_mut() {
+        *pixel = image::Rgb([255u8, 255u8, 255u8])
+    }
+
+    for x in (0..size).step_by(step as usize) {
+        for y in (0..size).step_by(step as usize) {
+            let left_to_right:bool = rand::thread_rng().gen_range(0.0, 1.0) >= 0.5;
+
+            if left_to_right {
+                //draw_line_segment_mut(&mut img, (context.current_x() as f32, context.current_y() as f32), ((context.current_x() + width) as f32, (context.current_y() + height) as f32), Rgb([0u8, 0u8, 0u8]));
+                draw_line_segment_mut(&mut img, (x as f32, y as f32), ((x + size) as f32, (y + size) as f32), Rgb([0u8, 0u8, 0u8]));
+                //*context.current_x_mut() = u16::from(x + width);
+                //*context.current_y_mut() = u16::from(y + height);
+            } else{
+                draw_line_segment_mut(&mut img, ((x + size) as f32, y as f32), (x as f32, (y + size) as f32), Rgb([0u8, 0u8, 0u8]));
+                //*context.current_x_mut() = u16::from(x + width);
+                // the above may be wrong, so i'm taking it out
+                //*context.current_y_mut() = u16::from(y + height);
+            }
         }
     }
-    */
+
     img.save("output.png").unwrap();
 }
